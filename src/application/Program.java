59bf0098adbe52be850b5f2e8cdd81acd250bfea
Adroaldo <hygino@msn.com>
2020-09-24 08:13:58 -0300

    Adicionado classe position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo");
-
+		Position pos = new Position(2, 3);
+		System.out.println(pos);
 	}
 
 }
